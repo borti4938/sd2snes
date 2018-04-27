cb6938451f768498b61a9b3004b4a488b82652db
borti4938 <borti4938@gmx.de>
2018-04-27 22:59:01 +0200

    scripts to grab new firmware builds (synthesis has to be run separately / FPGA binaries must exist and are not created by script)

---
+++ misc/create_new_fwfiles.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+toRootDir=".."
+targetDirName="sd2snes"
+
+mkdir ${targetDirName}
+
+
+# bs-x bin
+cp ${toRootDir}/bin/bsxpage.bin ${targetDirName}/.
+
+
+# FPGA files (generate main.bit files with Xilinx ISE design tools in advanced)
+utilsDirName="${toRootDir}/utils"
+rm -f ${utilsDirName}/rle
+
+gcc -o ${utilsDirName}/rle ${utilsDirName}/rle.c
+
+rle_cmd="${utilsDirName}/rle"
+verilogDirName="${toRootDir}/verilog/sd2snes"
+bitFile="main.bit"
+
+${rle_cmd} ${verilogDirName}/${bitFile} ${targetDirName}/fpga_base.bit
+${rle_cmd} ${verilogDirName}_cx4/${bitFile} ${targetDirName}/fpga_cx4.bit
+${rle_cmd} ${verilogDirName}_obc1/${bitFile} ${targetDirName}/fpga_obc1.bit
+
+
+# firmware
+srcDirName="${toRootDir}/src"
+
+make -C ${srcDirName}/utils
+make clean -C ${srcDirName}
+make -C ${srcDirName}
+
+cp ${srcDirName}/obj/firmware.img ${targetDirName}/.
+
+
+# snes menu
+snesDirName="${toRootDir}/snes"
+
+make clean -C ${srcDirName}
+make -C ${snesDirName}
+
+cp ${snesDirName}/menu.bin sd2snes/.
---
+++ misc/get_new_fwfiles.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+toRootDir=".."
+targetDirName="sd2snes"
+
+
+# bs-x bin
+cp ${toRootDir}/bin/bsxpage.bin ${targetDirName}/.
+
+
+# firmware (run make in src folder in advance)
+cp ${toRootDir}/src/obj/firmware.img ${targetDirName}/.
+
+
+# snes menu (run make in snes folder in advance)
+cp ${toRootDir}/snes/menu.bin sd2snes/.
+
+
+# FPGA files (generate main.bit files with Xilinx ISE design tools in advanced)
+rle_cmd="${toRootDir}/utils/rle"
+verilogDirName="${toRootDir}/verilog/sd2snes"
+bitFile="main.bit"
+
+${rle_cmd} ${verilogDirName}/${bitFile} ${targetDirName}/fpga_base.bit
+${rle_cmd} ${verilogDirName}_cx4/${bitFile} ${targetDirName}/fpga_cx4.bit
+${rle_cmd} ${verilogDirName}_obc1/${bitFile} ${targetDirName}/fpga_obc1.bit
